7cdae3b209232bf753fdbbc91279b694e6c4d992
jlggross <joaolggross@gmail.com>
2021-06-30 21:18:53 -0300

    feat: scaled definition. First signup page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,11 @@
 import * as React from 'react'
-import styled from 'styled-components'
 import { Theme } from './../components/Theme'
-import { background, color, fontSize } from './../components/Theme/styled'
+import { Signup } from './Signup'
 
-const BaseButton = ({ className, children }) => (
-	<div className={className}>{children}</div>
-)
-
-const BaseBox = ({ className, children }) => (
-	<div className={className}>{children}</div>
-)
-
-const Button = styled(BaseButton)`
-	${background}
-	${color}
-  ${fontSize}
-`
-
-const Box = styled(BaseBox)`
-	${background}
-	${color}
-  ${fontSize}
-`
-
-export const App = () => (
-	<Theme>
-		<Button bg="green" color="red" fontSize="large">
-			Button
-		</Button>
-		<Box bg="red" color="blue" fontSize="large">
-			Box
-		</Box>
-	</Theme>
-)
+export const App = () => {
+	return (
+		<Theme>
+			<Signup />
+		</Theme>
+	)
+}
