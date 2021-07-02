13e106a635170495d18b64bc9d807d1763980a70
jlggross <joaolggross@gmail.com>
2021-07-01 21:29:37 -0300

    feat: created form in signup with onchange and onsubmit functions

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,18 +1,53 @@
 import * as React from 'react'
+import { useState } from 'react'
 
 import { Box, Field, Button } from '~/components/'
 
 export const Signup = () => {
+	const [values, setValues] = useState({})
+
+	const onChange = (event) => {
+		setValues((prev) => ({
+			...prev,
+			[event.target.name]: event.target.value,
+		}))
+	}
+
+	const onSubmit = (event) => {
+		event.preventDefault()
+		console.log(values, event)
+	}
+
 	return (
 		<Box flex={1} flexbox="column" center>
 			<Box style={{ width: 380 }}>
-				<Field type="text" name="name" label="Nome" mb={3} />
-				<Field type="text" name="email" label="E-mail" mb={3} />
-				<Field type="password" name="password" label="Senha" mb={3} />
+				<form onSubmit={onSubmit}>
+					<Field
+						type="text"
+						name="name"
+						label="Nome"
+						mb={3}
+						onChange={onChange}
+					/>
+					<Field
+						type="text"
+						name="email"
+						label="E-mail"
+						mb={3}
+						onChange={onChange}
+					/>
+					<Field
+						type="password"
+						name="password"
+						label="Senha"
+						mb={3}
+						onChange={onChange}
+					/>
 
-				<Box flexbox center>
-					<Button>Registrar</Button>
-				</Box>
+					<Box flexbox center>
+						<Button onClick={onSubmit}>Registrar</Button>
+					</Box>
+				</form>
 			</Box>
 		</Box>
 	)
