07fe39409205e632d985511a10e61ff7e5b7af60
jlggross <joaolggross@gmail.com>
2021-10-26 22:35:19 -0300

    feat: persist user login if page refresh and logout button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,38 @@
 import * as React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Theme } from '~/components/'
+
 // import { Signup } from './Signup'
 import { Login } from './Login'
 
-const Dashboard = () => <div>Estou Logado!</div>
+const Dashboard = ({ onLogout }) => {
+	return (
+		<div>
+			Estou Logado!
+			<button onClick={onLogout}>Sair</button>
+		</div>
+	)
+}
 
 export const App = () => {
-	const [state, setState] = useState(false)
+	const [state, setState] = useState(() => {
+		const data = window.localStorage.getItem('auth')
+		return data && JSON.parse(data)
+	})
+
+	const logout = () => setState(false)
+
+	useEffect(() => {
+		window.localStorage.setItem('auth', state && JSON.stringify(state))
+	}, [state])
 
 	return (
 		<Theme>
-			{state?.user ? <Dashboard /> : <Login onSuccess={setState} />}
+			{state?.user ? (
+				<Dashboard onLogout={logout} />
+			) : (
+				<Login onSuccess={setState} />
+			)}
 		</Theme>
 	)
 }
